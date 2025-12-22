88bb0eae914ab1403c05d18fa71d15288d0bde93
kreateupcolleges <kreateupcolleges@gmail.com>
2025-12-22 23:44:01 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -12,6 +12,8 @@ if (!rootElement) {
 const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
